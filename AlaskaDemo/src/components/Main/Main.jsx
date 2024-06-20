5e841d067a8c719c93f2337c304e16a2271c86d3
Nataly Jaya Salazar <Nataliesalazar195@gmail.com>
2024-06-20 13:43:09 +0200

    Api
    add the gemini api

--- AlaskaDemo/src/components/Main/Main.jsx
+++ AlaskaDemo/src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import'./Main.css'
 import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
 
 
 
-const Main = () => {
+const{onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context)
+
     return (
         <div className='main'>
            <div className="nav">
@@ -13,6 +15,9 @@ const Main = () => {
 
            </div>
            <div className="main-container">
+
+              {!showResult
+              ?<>
             <div className="greet">
                 <p><span>Holiwis Kiwis!</span></p>
                 <p>Klk</p>
@@ -35,13 +40,38 @@ const Main = () => {
                   <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+
+              </>
+              : <div className="result">
+                  <div className="result-title">
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                  </div>
+                  <div className="result-data">
+                    <img src={assets.gemini_icon} alt="" />
+                    {loading
+                    ?<div className="loader">
+                      <img src={assets.loader} alt="" />
+                      <hr />
+                      <hr />
+                      <hr />
+                    </div>
+                    :
+                    <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+
+                  }
+                   
+                  </div>
+              </div>
+              }
+
             <div className="main-bottom">
                 <div className="search-box">
-                    <input type="text" placeholder="Escriba aqui" />
+                    <input onChange={(e)=>setInput(e.target.value) } value={input} type="text" placeholder="Escriba aqui" />
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img src={assets.send_icon} alt="" />
+                        <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
                     </div>
                     </div>
                     <p className="bottom-info">
@@ -52,6 +82,6 @@ const Main = () => {
            </div>
         </div>
     )
-}
+
 
 export default Main
